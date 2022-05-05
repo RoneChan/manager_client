7a4ce61b458e6dcbb5205ea42aa038ddb96f2a2e
kyo <604079771@qq.com>
2022-05-05 18:17:06 +0800

    modify ResultMatchConfig.vue file by mucheng

--- manager/src/components/ResultMatchConfig.vue
+++ manager/src/components/ResultMatchConfig.vue
@@ -8,7 +8,7 @@
               <br>
               <div id="exhibitorleft" >系统</div>
               <div id="exhibitorright">
-                <el-select v-model="systemVersion" placeholder="请选择">
+                <el-select v-model="systemName" placeholder="请选择">
                   <el-option
                     v-for="item in options"
                     :key="item.value"
@@ -43,7 +43,34 @@
           </el-col>
         </el-row>
         <div>
-          <el-button  type="success" icon="el-icon-edit" v-on:click="addResultDetail">添加映射</el-button>
+<!--          <el-button  type="success" icon="el-icon-edit" v-on:click="addResultDetail">添加映射</el-button>-->
+          <el-button  type="success" icon="el-icon-edit" v-on:click="dialogFormVisible = true">添加映射</el-button>
+          <el-dialog title="添加映射字典" :visible.sync="dialogFormVisible">
+            <el-form :model="form">
+              <el-form-item label="系统" :label-width="formLabelWidth">
+                <el-select id="addsys" v-model="form.matchingResultKey" placeholder="请选择系统">
+                  <el-option label="PAIC" value="PAIC"></el-option>
+                  <el-option label="AMTP" value="AMTP"></el-option>
+                  <el-option label="香港网银" value="香港网银"></el-option>
+                  <el-option label="香港掌银" value="香港掌银"></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item label="匹配关键字" :label-width="formLabelWidth">
+                <el-input v-model="form.systemName" autocomplete="off"></el-input>
+              </el-form-item>
+              <el-form-item label="预期结果详细描述" :label-width="formLabelWidth">
+                <el-input type="textarea"
+                          :rows="2" v-model="form.resultDetailDes" autocomplete="off"></el-input>
+              </el-form-item>
+              <el-form-item label="责任人" :label-width="formLabelWidth">
+                <el-input v-model="form.userName" autocomplete="off"></el-input>
+              </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+              <el-button type="primary" v-on:click="addResultDetail" @click="dialogFormVisible = false" >添 加</el-button>
+              <el-button @click="dialogFormVisible = false">取 消</el-button>
+            </div>
+          </el-dialog>
           <el-button  type="primary" icon="el-icon-search" v-on:click="getResultDetails">查询映射信息</el-button>
         </div>
         <el-table
@@ -90,7 +117,63 @@
 
 <script>
 export default {
-  name: "ResultMatchConfig"
+  name: "ResultMatchConfig",
+  data() {
+    return {
+      options: [{
+        value: 'PAIC',
+        label: 'PAIC'
+      }, {
+        value: 'AMTP',
+        label: 'AMTP'
+      }, {
+        value: '香港网银',
+        label: '香港网银'
+      }, {
+        value: '香港掌银',
+        label: '香港掌银'
+      }, {
+        value: 'AAAAAA',
+        label: 'AAAAAAA'
+      }],
+      systemName: '',//系统名称
+      matchingResultKey: '',//待匹配结果关键字
+      userName: '',// 规则字典维护人员
+      bgc: '', //样式
+      resultMatchDictionary: [{
+        systemName: '',
+        matchingResultKey: '',
+        resultDetailDes: '',
+        userName: '',
+        submitTime: '',
+        modifyTime: ''
+      }], // 接收查找的数据
+      dialogFormVisible: false,
+      form: {
+        systemName: '',
+        matchingResultKey: '',
+        resultDetailDes: '',
+        userName: ''
+      },
+      formLabelWidth: '130px'
+    }
+  },
+
+  methods: {
+    addResultDetail() {
+      console.log("addResultDetail");
+      console.log(this.systemName);
+      console.log(this.matchingResultKey);
+      console.log(this.userName);
+      console.log("YYYYYYYYYYYYY");
+      this.$message({
+        message:'添加结果映射成功！',
+        type: 'success'});
+      addMatchConfig(form.systemName,form.matchingResultKey,form.resultDetailDes).then(res => {
+        this.testRule = res.data.data
+      })
+    },
+  },
 }
 </script>
 
@@ -134,5 +217,15 @@ export default {
   float: left;
   width: 70%;
   height: 100%;
+}
+#addsys {
+  margin-left: 10px;
+}
+.el-dialog__footer {
+  padding: 10px 20px 50px;
+}
+
+.el-dialog__body {
+  padding: 30px 50px;
 }
 </style>
