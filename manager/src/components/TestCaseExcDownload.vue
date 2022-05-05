fa2c9124530700b49b5c7fcaf63dc20ae49db7f5
RoneChane <1317175728@qq.com>
2022-05-05 08:50:42 +0800

    新增文件下载界面，完成文件下载功能

---
+++ manager/src/components/TestCaseExcDownload.vue
@@ -0,0 +1,228 @@
+<template>
+  <div>
+    <el-container>
+      <el-main>
+          <el-row :gutter="20">
+            <el-col :span="6">
+              <div >
+                <br>
+                <div id="exhibitorleft" >系统</div>
+                <div id="exhibitorright">
+              <el-select v-model="systemVersion" placeholder="请选择">
+                <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+                </div>
+            </div>
+            </el-col>
+
+            <el-col :span="7">
+              <div>
+                <br>
+                <div id="exhibitorleft">交易名称</div>
+                <div id="exhibitorright">
+              <el-input
+                placeholder="输入交易名称" prefix-icon="el-icon-search" v-model="tradeName" id="testRule_tradeName_input" width="60px">
+              </el-input>
+                </div>
+            </div>
+            </el-col>
+            <el-col :span="7">
+              <div >
+                <br>
+                <div id="exhibitorleft" >文件类型</div>
+                <div id="exhibitorright">
+              <el-select v-model="fileType" placeholder="请选择">
+                <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+                </div>
+            </div>
+            </el-col>
+
+            <div>
+              <br>
+              <el-button type="primary" icon="el-icon-search" v-on:click="queryFile">查询文件</el-button>
+            </div>
+
+          </el-row>
+        <el-table
+          :data="fileData"
+          stripe
+          style="width: 100%">
+          <el-table-column
+            prop="systemVersion"
+            label="系统名称"
+            width="100">
+          </el-table-column>
+          <el-table-column
+            prop="tradeName"
+            label="交易名称"
+            width="100">
+          </el-table-column>
+          <el-table-column
+            prop="saveTime"
+            label="生成时间"
+            width="100">
+          </el-table-column>
+        <el-table-column label="操作">
+        <template slot-scope="scope">
+        <el-button
+          size="mini"
+          @click="downloadFile(scope.$index)">下载</el-button>
+      </template>
+        </el-table-column>
+        </el-table>
+      </el-main>
+    </el-container>
+  </div>
+
+
+
+
+</template>
+
+<script>
+//import {RuleGenerateTestCase} from "../api/get";
+import { findStudentScoreInfo } from "../api/get";
+import { ruleGenerateTestCase } from "../api/get";
+import { getTestRules } from "../api/get";
+
+export default {
+  name: "TestCaseExcDownload",
+  data() {
+    return {
+      options: [
+        {
+          value: "选项1",
+          label: "PAIC",
+        },
+        {
+          value: "选项2",
+          label: "AMTP",
+        },
+        {
+          value: "选项3",
+          label: "香港网银",
+        },
+        {
+          value: "选项4",
+          label: "香港掌银",
+        },
+
+      ],
+      fileType:[
+           {
+          value: "TC_PICT",
+          label: "PICT用例文件",
+        },
+        {
+          value: "TC_GW",
+          label: "GraphWalker用例文件",
+        },
+        {
+          value: "FILE_PICT",
+          label: "PICT规则文件",
+        },
+        {
+          value: "GRAPH_GW",
+          label: "Graphml图文件",
+        },
+
+      ],
+      systemVersion: "", //系统名称
+      tradeName: "", //交易名称
+      bgc: "", //样式
+      fileData: [
+          {
+              systemVersion: "OIBS",
+              tradeName:"sdfs",
+              saveTime:"20220102",
+              fileUrl: "http://127.0.0.1:8086/TC_PICT/20220426105505205.xls"
+          },
+           {
+              systemVersion: "dsfs",
+              tradeName:"sdffbfs",
+              saveTime:"20210102",
+              fileUrl: "http://127.0.0.1:8086/TC_GW/20220427112650749.graphml"
+          }
+      ], // 接收查找的数据
+    };
+  },
+
+  methods: {
+    // 返回
+    showScore_return() {
+      this.$router.back(-1);
+    },
+    // 鼠标悬停变色
+    mouseOver() {
+      this.bgc = "background-color: #cccccc;color: red";
+    },
+    mouseLeave() {
+      this.bgc = "";
+    },
+    queryFile(){
+    },
+    downloadFile(index) {
+
+        let url =this.fileData[index].fileUrl;
+        console.log(url,"urlurl");
+
+
+
+
+
+        const a = document.createElement('a')
+        a.href = url
+        a.click() // 点击下载
+       // document.body.removeChild(a) // 下载完成移除元素=
+    },
+  },
+};
+</script>
+
+<style scoped>
+.el-row {
+  margin-bottom: 20px;
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #1111bf;
+}
+.bg-purple {
+  background: #1111bf6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 2px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+#exhibitorleft {
+  float: left;
+  width: 30%;
+  height: 100%;
+  margin-top: 10px;
+}
+#exhibitorright {
+  float: left;
+  width: 70%;
+  height: 100%;
+}
+</style>
